30a9bc982a8403fba757664de2b66266d4ec4b1e
koh-tom <223271102+koh-tom@users.noreply.github.com>
2025-11-22 17:39:03 +0900

    加減算機能の追加
    add命令とsub命令

--- test.sh
+++ test.sh
@@ -18,5 +18,6 @@ assert() {
 
 assert 0 0
 assert 76 76
+assert 21 "5+20-4"
 
 echo OK
--- tmp.s
+++ tmp.s
@@ -1,6 +1,8 @@
 .intel_syntax noprefix
 .global main
 main:
-    mov rax, 76
+    mov rax, 5
+    add rax, 20
+    sub rax, 4
     ret
 .section .note.GNU-stack,"",@progbits
